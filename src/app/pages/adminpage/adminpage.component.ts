19d6cd2820d5afcc20e005da7f03741b1147e470
antoetbfr <antoebt@hotmail.com>
2020-05-20 15:14:21 +0200

    feat: admin cate list toggle

--- src/app/pages/adminpage/adminpage.component.ts
+++ src/app/pages/adminpage/adminpage.component.ts
@@ -7,19 +7,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminpageComponent implements OnInit {
 
+  private cateListCSS;
   public user = [
     1, 2, 3, 4 , 4 , 4 , 4 , 4 , 4 , 4, , 5 , 55 , 5 , 5, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
   ];
-
   public categories = ['users', 'video'];
 
   public selectedCategories = 'users';
   constructor() { }
 
   ngOnInit() {
+    this.cateListCSS = document.getElementById('categorie-list').style;
   }
 
   public transmission(categorie: string) {
+
+
     this.selectedCategories = categorie;
+    this.cateListCSS.display = 'none';
+  }
+
+  public openCategoriesList() {
+  this.cateListCSS.display = 'inline-block';
   }
 }
