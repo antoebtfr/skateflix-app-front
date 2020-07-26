aab2779333e2b62e5df127d9fd1b8521cce56a1c
antoebtfr <antoebtf@gmail.com>
2020-07-27 01:18:31 +0200

    pages: settings pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -12,7 +12,8 @@ const routes: Routes = [
   { path: 'validation', loadChildren: () => import('./pages/validation/validation.module').then(m => m.ValidationModule) },
   { path: '404-not-found' , loadChildren: () => import('./pages/page-not-found/page-not-found.module').then(m => m.PageNotFoundModule) },
   { path: 'user', loadChildren: () => import('./pages/profil/profil.module').then(m => m.ProfilModule) },
-  { path: 'admin', loadChildren: () => import('./pages/adminpage/adminpage.module').then(m => m.AdminpageModule) }
+  { path: 'admin', loadChildren: () => import('./pages/adminpage/adminpage.module').then(m => m.AdminpageModule) },
+  { path: 'settings', loadChildren: () => import('./pages/settings/settings.module').then(m => m.SettingsModule) }
 ];
 
 @NgModule({
