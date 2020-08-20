40254facfa15aca651142e9fb35166321243f1dc
antoebtfr <antoebt@hotmail.com>
2020-08-21 00:33:59 +0200

    fix: git conflict

--- src/app/pages/adminpage/adminpage-routing.module.ts
+++ src/app/pages/adminpage/adminpage-routing.module.ts
@@ -7,21 +7,15 @@ import { AdminpageComponent } from './adminpage.component';
 import { AdminSidebarComponent } from 'src/app/components/admin-sidebar/admin-sidebar.component';
 import { AdminListComponent } from './admin-list/admin-list.component';
 import { UsersListComponent } from './users-list/users-list.component';
-import { AdminEditModalComponent } from 'src/app/components/admin-edit-modal/admin-edit-modal.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'auth'},
   { path: 'auth', component: AdminAuthComponent},
   { path: 'dashboard', component: AdminpageComponent},
-<<<<<<< HEAD
   { path: 'ip', component: AdminSidebarComponent},
   { path: 'admin-list', component: AdminListComponent},
-  { path: 'chatbox', component: ChatboxComponent}
-=======
-  { path: 'ip', component: AdminEditModalComponent},
-  { path: 'admins-list', component: AdminListComponent},
+  { path: 'chatbox', component: ChatboxComponent},
   { path: 'users-list', component: UsersListComponent}
->>>>>>> 8f86eba75f8c6ba5d2b0cdb75d493d51b29eb6e4
 ];
 
 @NgModule({
--- src/app/pages/adminpage/adminpage.module.ts
+++ src/app/pages/adminpage/adminpage.module.ts
@@ -8,13 +8,10 @@ import { AdminAuthComponent } from './admin-auth/admin-auth.component';
 import { AdminSidebarComponent } from 'src/app/components/admin-sidebar/admin-sidebar.component';
 import { AdminListComponent } from './admin-list/admin-list.component';
 import { HttpClientModule } from '@angular/common/http';
-<<<<<<< HEAD
 import { ChatboxComponent } from './chatbox/chatbox.component';
-=======
 import { UsersListComponent } from './users-list/users-list.component';
 import { VideosListComponent } from './videos-list/videos-list.component';
 import { AdminEditModalComponent } from '../../components/admin-edit-modal/admin-edit-modal.component';
->>>>>>> 8f86eba75f8c6ba5d2b0cdb75d493d51b29eb6e4
 
 
 
@@ -24,13 +21,10 @@ import { AdminEditModalComponent } from '../../components/admin-edit-modal/admin
     AdminAuthComponent,
     AdminSidebarComponent,
     AdminListComponent,
-<<<<<<< HEAD
-    ChatboxComponent
-=======
+    ChatboxComponent,
     UsersListComponent,
     VideosListComponent,
     AdminEditModalComponent,
->>>>>>> 8f86eba75f8c6ba5d2b0cdb75d493d51b29eb6e4
   ],
 
   imports: [
