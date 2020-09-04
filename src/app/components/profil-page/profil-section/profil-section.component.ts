ab1d640c364f9f6b9fe7be59a18f1c81aba24ab3
antoetbfr <antoebt@hotmail.com>
2020-09-05 00:09:40 +0200

    feat: Display userconf keys in profil section

--- src/app/components/profil-page/profil-section/profil-section.component.ts
+++ src/app/components/profil-page/profil-section/profil-section.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { UserConf } from 'src/app/variable-globale/user-conf';
 
 @Component({
   selector: 'app-profil-section',
@@ -7,9 +8,15 @@ import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 })
 export class ProfilSectionComponent implements OnInit {
 
-  constructor() { }
+  constructor(private userConf: UserConf) { }
+  public currentUser;
 
   ngOnInit() {
+    this.actualyseUser();
+  }
+
+  private actualyseUser() {
+    this.currentUser = this.userConf.getUserInfo();
   }
 
 }
--- src/app/variable-globale/user-conf.ts
+++ src/app/variable-globale/user-conf.ts
@@ -4,6 +4,16 @@ import { Injectable } from '@angular/core';
 export class UserConf {
   private connected = true; // Variable managing the switch to connected / not connected mode of the site
 
+  private userFirstname = 'Pierre';
+  private userSurname = 'Croce';
+  private userNickname = 'Pierrot';
+  private userAge = 27;
+  private userNationality = 'France';
+  private userLocation = 'Bordeaux';
+  private userId = 2;
+  private userBio = 'Salut, je suis la bio de pieerot';
+  private userProfilPicture = 'https://nyjah.com/wp-content/uploads/2018/01/C_v2RQkWAAQ-4c3.jpg';
+
   public getConnexionStatus() { // Retrieve the status of the connected variable
     return this.connected;
   }
@@ -14,5 +24,21 @@ export class UserConf {
 
   public connection() { // Switch the site in Connected Mode
     this.connected = true;
+  }
+
+  public getUserInfo() {
+    const user = {
+      id: this.userId,
+      firstname: this.userFirstname,
+      surname: this.userSurname,
+      nickname: this.userNickname,
+      age: this.userAge,
+      location: this.userLocation,
+      nationality: this.userNationality,
+      profilPicture: this.userProfilPicture,
+      bio: this.userBio
+    };
+
+    return user;
   }
 }
