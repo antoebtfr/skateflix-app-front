da033c3b43ac12c6cdd8e39256e945ec44393a36
antoetbfr <antoebt@hotmail.com>
2020-09-05 01:07:33 +0200

    feat: Display video list >>> video section

--- src/app/components/profil-page/video-section/video-section.component.ts
+++ src/app/components/profil-page/video-section/video-section.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
+import { VideoService } from 'src/app/shared/service/video.service';
+import { UserConf } from 'src/app/variable-globale/user-conf';
 
 @Component({
   selector: 'app-video-section',
@@ -11,12 +13,19 @@ export class VideoSectionComponent implements OnInit {
   @ViewChild('filter_container', {static: true}) filterContainer: ElementRef;
 
   public changeFilterContainerHeight = false;
+  public topVideo;
+  public videoList = [];
+  private id: number;
 
-  constructor() { }
+  constructor(private videoService: VideoService, private userConf: UserConf) { }
 
   ngOnInit() {
     this.reduceFiltersContainer();
+    this.updateId();
+    this.updateVideoList();
+    this.checkVideo();
   }
+
 
   private reduceFiltersContainer() {
     const videoContainerCSS = document.getElementById('video-container');
@@ -28,7 +37,26 @@ export class VideoSectionComponent implements OnInit {
         this.changeFilterContainerHeight = false;
       }
     });
+  }
 
+  private updateVideoList() {
+     this.videoService.get().subscribe(data => this.videoList = data.filter(x => x.userId == this.id));
+  }
+
+  private updateId() {
+    this.id = this.userConf.getUserInfo().id;
+  }
+
+  private updateTopVideo() {
+    this.topVideo = this.videoList[this.videoList.length - 1];
+  }
+
+  private checkVideo() {
+    document.addEventListener('click', () => {
+      this.updateTopVideo();
+      console.log(this.videoList);
+      console.log(this.topVideo);
+    });
   }
 
 }
