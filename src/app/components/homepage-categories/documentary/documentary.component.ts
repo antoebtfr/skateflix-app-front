5bd12c8e91f7cc1f9ab16d8f5144a7154227ccc7
antoetbfr <antoebt@hotmail.com>
2020-09-04 23:48:15 +0200

    feat: Get videos

--- src/app/components/homepage-categories/documentary/documentary.component.ts
+++ src/app/components/homepage-categories/documentary/documentary.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { VideoService } from 'src/app/shared/service/video.service';
 
 @Component({
   selector: 'app-documentary',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DocumentaryComponent implements OnInit {
   public documentaryVideos = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-  constructor() { }
+  public testArray = [];
+  constructor(private videoService: VideoService) { }
 
   ngOnInit() {
+    this.videoService.get().subscribe(data => this.testArray = data.filter(x => x.category === 'documentaire'));
+    this.click();
+  }
+
+  private click() {
+    document.addEventListener('click', () => console.log(this.testArray));
   }
 
 }
--- src/app/shared/service/video.service.ts
+++ src/app/shared/service/video.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -16,6 +17,10 @@ export class VideoService {
     formData.append('file', object.file, object.file.name);
     return this.http
     .post(endpoint, formData);
+  }
+
+  public get(): Observable<any[]> {
+    return this.http.get<any[]>(this.URL);
   }
 
 }
